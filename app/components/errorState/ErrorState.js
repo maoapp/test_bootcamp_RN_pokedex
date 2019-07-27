20f06dd248e88be0dd475ca9aa690688c9035b64
Mauricio Arroyave Alzate <maoarroya@gmail.com>
2019-07-27 09:15:21 -0500

    adding errorState component

---
+++ app/components/errorState/ErrorState.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ErrorState = () => (
+  <View>
+    <Text>Error fetching the data</Text>
+  </View>
+)
+
+export default ErrorState;
